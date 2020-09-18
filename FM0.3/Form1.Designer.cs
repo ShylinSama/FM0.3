3f35fd4bc82b94b598055d896f35f07ddb89363c
E6892@365office.site <E6892@365office.site>
2020-09-18 16:49:10 +0800

    添加项目文件。

---
+++ FM0.3/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace FM0._3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_Data = new System.Windows.Forms.TextBox();
+            this.txt_ReturnCode = new System.Windows.Forms.TextBox();
+            this.lbl_RetrunCode = new System.Windows.Forms.Label();
+            this.lbl_Data = new System.Windows.Forms.Label();
+            this.btn_Open = new System.Windows.Forms.Button();
+            this.btn_Close = new System.Windows.Forms.Button();
+            this.btn_ReadDeviceBlock2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txt_Data
+            // 
+            this.txt_Data.AcceptsReturn = true;
+            this.txt_Data.Location = new System.Drawing.Point(22, 299);
+            this.txt_Data.Multiline = true;
+            this.txt_Data.Name = "txt_Data";
+            this.txt_Data.ReadOnly = true;
+            this.txt_Data.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txt_Data.Size = new System.Drawing.Size(753, 125);
+            this.txt_Data.TabIndex = 51;
+            this.txt_Data.TabStop = false;
+            // 
+            // txt_ReturnCode
+            // 
+            this.txt_ReturnCode.Location = new System.Drawing.Point(104, 251);
+            this.txt_ReturnCode.Name = "txt_ReturnCode";
+            this.txt_ReturnCode.ReadOnly = true;
+            this.txt_ReturnCode.Size = new System.Drawing.Size(671, 21);
+            this.txt_ReturnCode.TabIndex = 52;
+            this.txt_ReturnCode.TabStop = false;
+            // 
+            // lbl_RetrunCode
+            // 
+            this.lbl_RetrunCode.Location = new System.Drawing.Point(24, 251);
+            this.lbl_RetrunCode.Name = "lbl_RetrunCode";
+            this.lbl_RetrunCode.Size = new System.Drawing.Size(72, 16);
+            this.lbl_RetrunCode.TabIndex = 53;
+            this.lbl_RetrunCode.Text = "Return Code:";
+            // 
+            // lbl_Data
+            // 
+            this.lbl_Data.Location = new System.Drawing.Point(24, 275);
+            this.lbl_Data.Name = "lbl_Data";
+            this.lbl_Data.Size = new System.Drawing.Size(72, 16);
+            this.lbl_Data.TabIndex = 54;
+            this.lbl_Data.Text = "Data:";
+            // 
+            // btn_Open
+            // 
+            this.btn_Open.Location = new System.Drawing.Point(628, 22);
+            this.btn_Open.Name = "btn_Open";
+            this.btn_Open.Size = new System.Drawing.Size(128, 32);
+            this.btn_Open.TabIndex = 80;
+            this.btn_Open.Text = "Open";
+            this.btn_Open.Click += new System.EventHandler(this.btn_Open_Click);
+            // 
+            // btn_Close
+            // 
+            this.btn_Close.Location = new System.Drawing.Point(628, 70);
+            this.btn_Close.Name = "btn_Close";
+            this.btn_Close.Size = new System.Drawing.Size(128, 32);
+            this.btn_Close.TabIndex = 81;
+            this.btn_Close.Text = "Close";
+            this.btn_Close.Click += new System.EventHandler(this.btn_Close_Click);
+            // 
+            // btn_ReadDeviceBlock2
+            // 
+            this.btn_ReadDeviceBlock2.Location = new System.Drawing.Point(628, 122);
+            this.btn_ReadDeviceBlock2.Name = "btn_ReadDeviceBlock2";
+            this.btn_ReadDeviceBlock2.Size = new System.Drawing.Size(128, 32);
+            this.btn_ReadDeviceBlock2.TabIndex = 82;
+            this.btn_ReadDeviceBlock2.Text = "ReadDeviceBlock2";
+            this.btn_ReadDeviceBlock2.Click += new System.EventHandler(this.btn_ReadDeviceBlock2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_ReadDeviceBlock2);
+            this.Controls.Add(this.btn_Open);
+            this.Controls.Add(this.btn_Close);
+            this.Controls.Add(this.txt_ReturnCode);
+            this.Controls.Add(this.lbl_RetrunCode);
+            this.Controls.Add(this.lbl_Data);
+            this.Controls.Add(this.txt_Data);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.TextBox txt_Data;
+        internal System.Windows.Forms.TextBox txt_ReturnCode;
+        internal System.Windows.Forms.Label lbl_RetrunCode;
+        internal System.Windows.Forms.Label lbl_Data;
+        internal System.Windows.Forms.Button btn_Open;
+        internal System.Windows.Forms.Button btn_Close;
+        internal System.Windows.Forms.Button btn_ReadDeviceBlock2;
+    }
+}
+
---
+++ FM0.3/Form1.cs
@@ -0,0 +1,274 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FM0._3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        #region "Additional initialization (including Form_Load processing)"
+        /*The declaration of instance value for ACT controls************************/
+        // When you use Dot controls by 'References', you should program as follows;
+        private ActUtlTypeLib.ActUtlTypeClass lpcom_ReferencesUtlType;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            /* Create instance for ACT Controls*************************************/
+            lpcom_ReferencesUtlType = new ActUtlTypeLib.ActUtlTypeClass();
+
+            /* Set EventHandler for ACT Controls************************************/
+            // Create EventHandler(ActUtlType)
+            lpcom_ReferencesUtlType.OnDeviceStatus +=
+                new ActUtlTypeLib._IActUtlTypeEvents_OnDeviceStatusEventHandler(ActUtlType1_OnDeviceStatus);
+            /**************************************************************************/
+
+        }
+        #endregion
+
+        #region  ""Processing of Open button"
+        private void btn_Open_Click(object sender, EventArgs e)
+        {
+            int iReturnCode;				//Return code
+            int iLogicalStationNumber=0;		//LogicalStationNumber for ActUtlType
+
+            //Displayed output data is cleared.
+            ClearDisplay();
+
+            //
+            //Processing of Open method
+            //
+            try
+            {
+
+                //When ActUtlType is selected by the radio button,
+
+                //Set the value of 'LogicalStationNumber' to the property.
+                lpcom_ReferencesUtlType.ActLogicalStationNumber = iLogicalStationNumber;
+
+                //Set the value of 'Password'.
+                lpcom_ReferencesUtlType.ActPassword = "";
+
+                //The Open method is executed.
+                iReturnCode = lpcom_ReferencesUtlType.Open();
+
+            }
+            //Exception processing
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message,
+                                  Name, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            //The return code of the method is displayed by the hexadecimal.
+            txt_ReturnCode.Text = String.Format("0x{0:x8} [HEX]", iReturnCode) + DateTime.Now.ToString();
+        }
+        #endregion
+
+        #region  "Processing of Close button"
+        private void btn_Close_Click(object sender, EventArgs e)
+        {
+            int iReturnCode;	//Return code
+
+            //Displayed output data is cleared.
+            ClearDisplay();
+
+            //
+            //Processing of Close method
+            //
+            try
+            {
+                //When ActUtlType is selected by the radio button,
+
+                //The Close method is executed.
+                iReturnCode = lpcom_ReferencesUtlType.Close();
+
+            }
+
+            //Exception processing
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message,
+                                  Name, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            //The return code of the method is displayed by the hexadecimal.
+            txt_ReturnCode.Text = String.Format("0x{0:x8} [HEX]", iReturnCode)+DateTime.Now.ToString();
+
+        }
+        #endregion
+
+        #region "Processing of OnDeviceStatus for ActUtlType Controle"
+        private void ActUtlType1_OnDeviceStatus(String szDevice, int iData, int iReturnCode)
+        {
+            System.String[] arrData;	        //Array for 'Data'
+            //Assign the array for the read data.
+            arrData = new System.String[txt_Data.Lines.Length + 1];
+
+            //Copy the read data to the 'arrData'.
+            Array.Copy(txt_Data.Lines, arrData, txt_Data.Lines.Length);
+
+            //Add the content of new event to arrData.
+            arrData[txt_Data.Lines.Length]
+            = String.Format("OnDeviceStatus event by ActUtlType [{0}={1}]", szDevice, iData);
+
+            //The new 'Data' is displayed.
+            txt_Data.Lines = arrData;
+
+            //The return code of the method is displayed by the hexadecimal.
+            txt_ReturnCode.Text = String.Format("0x{0:x8}", iReturnCode) + DateTime.Now.ToString();
+
+        }
+        #endregion
+
+        #region " Processing of clear displayed output data"
+        private void ClearDisplay()
+        {
+            //Clear TextBox of 'ReturnCode','Data'
+            txt_ReturnCode.Text = "";
+            txt_Data.Text = "";
+        }
+        #endregion
+
+        #region "Processing of ReadDeviceBlock2 button"
+        private void btn_ReadDeviceBlock2_Click(object sender, EventArgs e)
+        {
+            int iReturnCode;				//Return code
+            String szDeviceName = "";		//List data for 'DeviceName'
+            int iNumberOfData = 0;			//Data for 'DeviceSize'
+            short[] arrDeviceValue;		    //Data for 'DeviceValue'
+
+            //Displayed output data is cleared.
+            ClearDisplay();
+
+            //Get the list of 'DeviceName'.
+            //  Join each line(StringType array) of 'DeviceName' by the separator '\n',
+            //  and create a joined string data.
+            szDeviceName ="F0";
+
+            iNumberOfData = 188;
+
+            //Assign the array for 'DeviceValue'.
+            arrDeviceValue = new short[iNumberOfData];
+
+            //
+            //Processing of ReadDeviceBlock2 method
+            //
+            try
+            {
+
+                //When ActUtlType is selected by the radio button,
+
+                //The ReadDeviceBlock2 method is executed.
+                iReturnCode = lpcom_ReferencesUtlType.ReadDeviceBlock2(szDeviceName,
+                                                                iNumberOfData,
+                                                                out arrDeviceValue[0]);
+            }
+
+            //Exception processing			
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message, Name,
+                                  MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            //The return code of the method is displayed by the hexadecimal.
+            txt_ReturnCode.Text = String.Format("0x{0:x8}", iReturnCode);
+
+            //
+            //Display the read data
+            //
+            //When the ReadDeviceRandom2 method is succeeded, display the read data.
+            if (iReturnCode == 0)
+            {
+                //对获得的F报警进行处理
+                //当总数和不为0时，说明有报错发生
+
+                //对数组求和
+                int sum = 0;
+                for (int i = 0; i < arrDeviceValue.Length; i++)
+                {
+                    sum += arrDeviceValue[i];
+                }
+
+                //判断数组和是否为0
+                if (sum==0)
+                {
+                    //数据和为0，证明F0~F3007没有ON，未发生报警
+                    Console.WriteLine("未发生报错");
+                }
+                else
+                {
+                    //调用函数获取报警数据数组
+                    List<int> F_Alarm_Code = GetF_Alarm(arrDeviceValue);
+
+                }
+            }
+        }
+        #endregion
+
+        #region 将获得报警字符串转换成正确顺序的数组
+
+        //输入  从PLC读取到的short 数组
+        //输出  报警的代码 list<int>
+        private List<int> GetF_Alarm(short[] arrDeviceValue)
+        { 
+            //对数组数据进行解析
+            //将10进制数变换成16位2进制
+            string strResult = string.Empty;
+            string strTemp;
+            string strTemp_True;
+            Char[] chArr_true;
+            List<int> F_Alarm_Code;
+            for (int i = 0; i < arrDeviceValue.Length; i++)
+            {
+                //将10进制转换成16位2进制
+                strTemp = System.Convert.ToString(arrDeviceValue[i], 2);
+                strTemp = strTemp.Insert(0, new string('0', 16 - strTemp.Length));
+
+                //将数据前后互换得到正确顺序的字符串
+                Char[] chArr = strTemp.ToCharArray();
+                chArr_true = new Char[16];
+                for (int m = 0; m < chArr.Length; m++)
+                {
+                    chArr_true[m] = chArr[15 - m];
+                }
+                strTemp_True = new String(chArr_true); ;
+                strResult += strTemp_True;
+            }
+
+            //将字符串转换成Char
+            char[] F_报警集合 = strResult.ToArray();
+
+            //初始化报警代码列表
+            F_Alarm_Code = new List<int>();
+
+            //将报警代码添加到报警代码列表中
+            for (int i = 0; i < F_报警集合.Length; i++)
+            {
+                if (F_报警集合[i]=='1')
+                {
+                    F_Alarm_Code.Add(i);
+                }
+            }
+
+            return F_Alarm_Code;
+        }
+
+        #endregion
+
+    }
+}
